827fb5e5c3ba0abc4cc9854ed8662ab3c2b6b45b
mikael.tallbo <mikael.tallbo@gmail.com>
2020-04-26 20:55:02 +0200

    Add productservice

---
+++ Webshop/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop.Models;
+
+namespace Webshop.Services
+{
+    public class ProductService : IProductService
+    {
+        private List<Product> Products = new List<Product>();
+        public ProductService()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Products.Add(new Product { Id = new Guid(), Description = "En kaffekopp man kan dricka kaffe ur", ImageURL = "https://networkprojectum.se/wp-content/uploads/2017/11/placeholder.png", Name = "Kaffekopp", Price = 200 });
+            }
+        }
+
+        public Product GetById(Guid id)
+        {
+            return Products.FirstOrDefault(p => p.Id == id);
+        }
+        public List<Product> GetAll()
+        {
+            return Products;
+        }
+    }
+}
