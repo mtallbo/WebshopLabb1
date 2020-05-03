f3e2ad21e8f408053948d4475163214f27f5dc85
mikael.tallbo <mikael.tallbo@gmail.com>
2020-05-03 16:42:27 +0200

    productdetails
    add productdetails page
    small fixes

--- Webshop/Controllers/ProductController.cs
+++ Webshop/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Webshop.Services;
@@ -21,6 +22,16 @@ namespace Webshop.Controllers
             ViewBag.sessionv =  HttpContext.Session.GetString("User");
             var products = _productService.GetAll();
             return View(products);
+        }
+
+        public IActionResult Details(Guid productId)
+        {
+            var product = _productService.GetById(productId);
+            if (product == null) 
+            {
+                return NotFound();
+            }
+            return View(product);
         }
     }
 }
