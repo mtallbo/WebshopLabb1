c66dab1579544de1ad32ff5024f0b4d854040b84
mikael.tallbo <mikael.tallbo@gmail.com>
2020-05-04 12:15:33 +0200

    custom fields for profile
    *LOTS OF SCAFFOLDING FILES
    *custom field for profile (city, adress, postal)

---
+++ Webshop/Data/Migrations/20200504093951_customuser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Webshop.Data.Migrations
+{
+    public partial class customuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Adress",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PostalCode",
+                table: "AspNetUsers",
+                nullable: true,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Adress",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Webshop/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
