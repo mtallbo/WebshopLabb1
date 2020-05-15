ecaeb32ded11b90071fd39839c7b23a4ed418fad
mikael.tallbo <mikael.tallbo@gmail.com>
2020-05-15 08:28:10 +0200

    integration testing for productservice

---
+++ Webshop.TestLib/Services/ProductServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Webshop.TestLib.Services
+{
+    public class ProductServiceTest
+    {
+        [Fact]
+        public async Task GetAll_Return200()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var response = await client.GetAsync("/api/product");
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+        [Fact]
+        public async Task GetSingleRandomGuid_Return200()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                Guid g = Guid.NewGuid();
+                var response = await client.GetAsync($"/api/product/{g}");
+                response.EnsureSuccessStatusCode();
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+        [Fact]
+        public async Task GetSingleNotAGuid_Return400()
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                Guid g = Guid.NewGuid();
+                var response = await client.GetAsync($"/api/product/123");
+                Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            }
+        }
+    }
+}
---
+++ Webshop.TestLib/Services/TestClientProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Webshop.ProductAPI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Webshop.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Webshop.TestLib.Services
+{
+    public class TestClientProvider : IDisposable
+    {
+        public TestServer Server { get; private set; }
+        public HttpClient Client { get; private set; }
+        public TestClientProvider()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            WebHostBuilder webHostBuilder = new WebHostBuilder();
+            webHostBuilder.ConfigureServices(s => s.AddDbContext<ProductContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))));
+
+            webHostBuilder.UseStartup<Startup>();
+
+            Server = new TestServer(webHostBuilder);
+
+            Client = Server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Server?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
